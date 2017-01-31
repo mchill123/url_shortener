c51044595d9a2539a69a948e9dd2c07e66dafd19
Mckinley <mckinley.s.hill@gmail.com>
2017-01-31 16:39:12 +0000

    json response & shortened url generator

--- api.js
+++ api.js
@@ -3,9 +3,24 @@
 module.exports = function(app, db){
     app.get('/new/:url*', handleUrl);
         
+    function shortenSave(nurl){
+        var sUrl = 1000+Math.floor(Math.random()*9999);
+        var sObj = { sUrl: nurl };
+        var collection = db.collection('sites');
+        collection.insert(sObj);
+        return sUrl;
+        
+    }
+    
     function handleUrl(req, res){
-        var nUrl = req.params.url;
-        res.send(req.get('referer'));
+        var nUrl =req.url.slice(5);
+        var oUrl = req.protocol+"://"+req.get('host')+req.url;
+        var sUrl = req.protocol+"://"+req.get('host')+shortenSave(nUrl);
+        var dObj = {
+            'Original URL': oUrl,
+            'Shortened URL': sUrl
+        };
+        res.send(dObj);
         
         
     }
