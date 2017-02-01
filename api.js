946d860fcb57df0475a0f973417972ed534cd01f
Mckinley <mckinley.s.hill@gmail.com>
2017-02-01 18:07:32 +0000

    T

--- api.js
+++ api.js
@@ -3,11 +3,11 @@ var path = require('path');
 
 module.exports = function(app, db){
     app.all('/new/:url*', handleUrl);
-    app.all('/*', searchReturn);
     app.all('/', function(req,res){
     var filename = path.join(__dirname, 'index.html');
     res.sendFile(filename);
 });
+    app.all('/*', searchReturn);
    
         
    
